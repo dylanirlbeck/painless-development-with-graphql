6123ec0968e5ff12d140622144e2c6ca0035b6f2
Mitch Masia <mjmasia@gmail.com>
2019-10-26 13:25:30 -0500

    Adds looking back slides

---
+++ slides/TableOfContents/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const createStyleGetter = activeStep => step => ({
+  opacity: activeStep === step ? 1 : 0.2,
+});
+
+const steps = [
+  { title: "Looking back" },
+  { title: "Different paradigm" },
+  { title: "Core Tenets" },
+  { title: "Moving faster" },
+  { title: "Advanced" },
+];
+
+const TableOfContents = props => {
+  const { activeStep } = props;
+
+  const getStyle = createStyleGetter(activeStep);
+
+  return (
+    <div>
+      {steps.map((step, index) => {
+        const { title } = step;
+        return <h2 style={getStyle(index)}>{title}</h2>;
+      })}
+    </div>
+  );
+};
+
+export default TableOfContents;
